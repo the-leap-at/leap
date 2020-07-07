8260aab5b9d1e59b8932998a6e4494e0f8d7b8db
IacobSon <dorian.iacobescu@gmail.com>
2020-07-07 22:59:55 +0200

    Post state module

--- lib/leap_web/components/main/post.ex
+++ lib/leap_web/components/main/post.ex
@@ -1,56 +1,24 @@
 defmodule LeapWeb.Components.Main.Post do
   use LeapWeb, :component
-  use TypedStruct
 
-  alias LeapWeb.Components.Main.Post.Mutation
-  alias Leap.Content
-  alias Leap.Content.Schema.Post
-  alias Leap.Group
-  alias Leap.Group.Schema.Category
-  alias Leap.Accounts.Schema.User
-
-  defmodule State do
-    @moduledoc false
-
-    @typedoc "Post state"
-    typedstruct do
-      field :id, String.t(), enforce: true
-      field :module, module(), enforce: true
-      field :current_user, User.t()
-      field :post, Post.t(), enforce: true
-      field :post_changeset, Ecto.Changeset.t(Post.t())
-      field :post_behaviour, atom(), default: :show_post, enforce: true
-      field :categories, [Category.t()], default: []
-    end
-  end
+  alias LeapWeb.Components.Main.Post.State
 
   def mount(socket) do
     {:ok, socket}
   end
 
-  def update(%{action: :init, id: id, post: post, current_user: current_user}, socket) do
-    changeset = Content.change_post(post)
-
-    state = %State{
-      id: id,
-      module: __MODULE__,
-      current_user: current_user,
-      post: post,
-      post_changeset: changeset,
-      post_behaviour: post_behaviour(post),
-      categories: Group.all(Category)
-    }
-
-    {:ok, assign(socket, :state, state)}
-  end
-
-  def update(%{action: action, payload: attrs}, %{assigns: %{state: state}} = socket) do
-    mutation = Mutation.commit(action, attrs, state)
+  def update(%{action: :init} = assigns, socket) do
+    mutation = State.init(assigns)
     response(mutation, socket)
   end
 
-  defp response({:ok, {%State{} = state, {type, message}}}, socket) do
-    send_notification(state.id, type, message)
+  def update(%{action: action, payload: attrs}, %{assigns: %{state: state}} = socket) do
+    mutation = State.commit(action, attrs, state)
+    response(mutation, socket)
+  end
+
+  defp response({:ok, {%State{} = state, {message_type, message}}}, socket) do
+    send_notification(state.id, message_type, message)
     {:ok, assign(socket, :state, state)}
   end
 
@@ -78,8 +46,4 @@ defmodule LeapWeb.Components.Main.Post do
       state: state
     )
   end
-
-  defp post_behaviour(%Post{state: :new}), do: :edit_post
-  defp post_behaviour(%Post{state: :draft}), do: :edit_post
-  defp post_behaviour(%Post{state: :published}), do: :show_post
 end
--- lib/leap_web/components/main/post/state.ex
+++ lib/leap_web/components/main/post/state.ex
@@ -1,19 +1,52 @@
-defmodule LeapWeb.Components.Main.Post.Mutation do
+defmodule LeapWeb.Components.Main.Post.State do
   @moduledoc """
   Mutations for Post
-  Need to make distinction between mutations that update the changeset or not
   """
 
-  alias LeapWeb.Components.Main.Post.State
+  use TypedStruct
+
+  alias __MODULE__
+  alias LeapWeb.Components.State, as: StateBehaviour
 
   alias Leap.Content
   alias Leap.Content.Schema.Post
   alias Leap.Group
   alias Leap.Group.Schema.Category
+  alias Leap.Accounts.Schema.User
+
+  @behaviour StateBehaviour
+
+  @typedoc "Post state"
+  typedstruct do
+    field :id, String.t(), enforce: true
+    field :module, module(), enforce: true
+    field :current_user, User.t()
+    field :post, Post.t(), enforce: true
+    field :post_changeset, Ecto.Changeset.t(Post.t())
+    field :post_behaviour, atom(), default: :show_post, enforce: true
+    field :categories, [Category.t()], default: []
+  end
 
   defguard is_present(value) when is_binary(value) and bit_size(value) > 0
 
-  @spec commit(atom(), any(), State.t()) :: State.t()
+  @impl StateBehaviour
+  def init(%{id: id, post: post, current_user: current_user}) do
+    changeset = Content.change_post(post)
+
+    state = %State{
+      id: id,
+      module: LeapWeb.Components.Main.Post,
+      current_user: current_user,
+      post: post,
+      post_changeset: changeset,
+      post_behaviour: post_behaviour(post),
+      categories: Group.all(Category)
+    }
+
+    {:ok, state}
+  end
+
+  @impl StateBehaviour
   def commit(:update_post, post_params, state) do
     case Content.update_post(state.current_user, state.post, post_params) do
       {:ok, post} ->
@@ -66,7 +99,7 @@ defmodule LeapWeb.Components.Main.Post.Mutation do
 
       {:error, %Ecto.Changeset{} = changeset} ->
         state = %State{state | post_changeset: changeset}
-        {:ok, state}
+        {:ok, {state, {:danger, "Error publishing post"}}}
 
       {:error, message} ->
         {:error, message}
@@ -100,4 +133,8 @@ defmodule LeapWeb.Components.Main.Post.Mutation do
   defp with_preloads(%Post{} = post) do
     Content.with_preloads(post, [:category], force: true)
   end
+
+  defp post_behaviour(%Post{state: :new}), do: :edit_post
+  defp post_behaviour(%Post{state: :draft}), do: :edit_post
+  defp post_behaviour(%Post{state: :published}), do: :show_post
 end
